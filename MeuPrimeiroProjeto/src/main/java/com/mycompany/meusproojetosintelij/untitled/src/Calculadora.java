9556e37e0bc23cc1192a661fc72681c7098ed7f9
FERNAND FERNANDES OLIVEIRA <fernandfernandesoliveira@gmail.com>
2026-06-23 15:49:32 -0300

    git add .

---
+++ MeuPrimeiroProjeto/src/main/java/com/mycompany/meusproojetosintelij/untitled/src/Calculadora.java
@@ -0,0 +1,14 @@
+public class mechendo_com_parametros {
+
+    public static void main(String[]args) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+
+        System.out.println(args.length);
+    }
+
+    static void sum (int x, int y){
+            System.out.println(x + y);
+        }
+
+}
